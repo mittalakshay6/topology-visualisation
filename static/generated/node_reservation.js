1b645758c7b54839e9aa3fcd2196830fc2a8ea29
Akshay Mittal <akshmitt@cisco.com>
2021-05-11 22:03:05 +0530

    formal commit

--- static/generated/node_reservation.js
+++ static/generated/node_reservation.js
@@ -4,7 +4,7 @@ var nodeReservationData = {
     "Arches_2013": "OCCUPIED",
     "Bigbend_2017": "OCCUPIED",
     "Bigbend_2020": "FREE",
-    "Bigbend_2021": "ETA EXPIRED",
+    "Bigbend_2021": "FREE",
     "DDC_2025": "FREE",
     "Darwin_2024": "OCCUPIED",
     "Eyrie_2023": "OCCUPIED",
@@ -17,13 +17,13 @@ var nodeReservationData = {
     "RSP4_2034_2035": "OCCUPIED",
     "RSP4_2039": "OCCUPIED",
     "RSP4_2040_2041": "ETA EXPIRED",
-    "Taihu_2027": "ETA EXPIRED",
+    "Taihu_2027": "OCCUPIED",
     "Tortin_2005": "ETA EXPIRED",
     "Tortin_2008": "OCCUPIED",
     "Tortin_2009": "FREE",
     "Tortin_2011": "ETA EXPIRED",
-    "Tortin_2012": "ETA EXPIRED",
-    "Tortin_2018": "ETA EXPIRED",
+    "Tortin_2012": "OCCUPIED",
+    "Tortin_2018": "FREE",
     "Tortin_2019": "FREE",
     "Turin_2004": "FREE",
     "Turin_2014": "OCCUPIED",
@@ -32,4 +32,4 @@ var nodeReservationData = {
     "Zermatt_2010": "FREE"
 };
 
-var reservation_timestamp = "10/May/2021 23:27:11"
+var reservation_timestamp = "11/May/2021 21:47:07"
---
+++ static/generated/topology.js
@@ -0,0 +1,640 @@
+
+
+var topologyData = {
+    "links": [
+        {
+            "id": 0,
+            "source": 8,
+            "srcDevice": "RSP4_2039",
+            "srcIfName": "TenGigE0/4/0/5",
+            "target": 9,
+            "tgtDevice": "Tortin_2008",
+            "tgtIfName": "TenGigE0/0/0/5"
+        },
+        {
+            "id": 1,
+            "source": 8,
+            "srcDevice": "RSP4_2039",
+            "srcIfName": "TenGigE0/4/0/6",
+            "target": 9,
+            "tgtDevice": "Tortin_2008",
+            "tgtIfName": "TenGigE0/0/0/4"
+        },
+        {
+            "id": 2,
+            "source": 10,
+            "srcDevice": "Tortin_2005",
+            "srcIfName": "TenGigE0/0/0/6",
+            "target": 11,
+            "tgtDevice": "Bigbend_2017",
+            "tgtIfName": "TenGigE0/0/0/4"
+        },
+        {
+            "id": 3,
+            "source": 10,
+            "srcDevice": "Tortin_2005",
+            "srcIfName": "TenGigE0/0/0/10",
+            "target": 11,
+            "tgtDevice": "Bigbend_2017",
+            "tgtIfName": "TenGigE0/0/0/22"
+        },
+        {
+            "id": 4,
+            "source": 10,
+            "srcDevice": "Tortin_2005",
+            "srcIfName": "TenGigE0/0/0/13",
+            "target": 11,
+            "tgtDevice": "Bigbend_2017",
+            "tgtIfName": "TenGigE0/0/0/15"
+        },
+        {
+            "id": 5,
+            "source": 10,
+            "srcDevice": "Tortin_2005",
+            "srcIfName": "GigabitEthernet0/0/0/14",
+            "target": 12,
+            "tgtDevice": "Eyrie_2023",
+            "tgtIfName": "GigabitEthernet0/0/0/10"
+        },
+        {
+            "id": 6,
+            "source": 10,
+            "srcDevice": "Tortin_2005",
+            "srcIfName": "TenGigE0/0/0/16",
+            "target": 13,
+            "tgtDevice": "Darwin_2024",
+            "tgtIfName": "TenGigE0/0/0/18"
+        },
+        {
+            "id": 7,
+            "source": 14,
+            "srcDevice": "UFI_2_2022",
+            "srcIfName": "HundredGigE0/0/0/4",
+            "target": 15,
+            "tgtDevice": "UFI_2025",
+            "tgtIfName": "HundredGigE0/0/0/4"
+        },
+        {
+            "id": 8,
+            "source": 14,
+            "srcDevice": "UFI_2_2022",
+            "srcIfName": "HundredGigE0/0/0/12",
+            "target": 16,
+            "tgtDevice": "Pyke_2007",
+            "tgtIfName": "HundredGigE0/0/0/13"
+        },
+        {
+            "id": 9,
+            "source": 17,
+            "srcDevice": "Tortin_2018",
+            "srcIfName": "TenGigE0/0/0/11",
+            "target": 18,
+            "tgtDevice": "Bigbend_2021",
+            "tgtIfName": "TenGigE0/0/0/11"
+        },
+        {
+            "id": 10,
+            "source": 17,
+            "srcDevice": "Tortin_2018",
+            "srcIfName": "HundredGigE0/0/1/0",
+            "target": 19,
+            "tgtDevice": "DDC_2025",
+            "tgtIfName": "HundredGigE0/0/0/0"
+        },
+        {
+            "id": 11,
+            "source": 17,
+            "srcDevice": "Tortin_2018",
+            "srcIfName": "HundredGigE0/0/1/1",
+            "target": 19,
+            "tgtDevice": "DDC_2025",
+            "tgtIfName": "HundredGigE1/0/0/0"
+        },
+        {
+            "id": 12,
+            "source": 20,
+            "srcDevice": "Pyke_2006",
+            "srcIfName": "HundredGigE0/0/0/0",
+            "target": 9,
+            "tgtDevice": "Tortin_2008",
+            "tgtIfName": "HundredGigE0/0/1/0"
+        },
+        {
+            "id": 13,
+            "source": 20,
+            "srcDevice": "Pyke_2006",
+            "srcIfName": "HundredGigE0/0/0/9",
+            "target": 15,
+            "tgtDevice": "UFI_2025",
+            "tgtIfName": "HundredGigE0/0/0/21"
+        },
+        {
+            "id": 14,
+            "source": 20,
+            "srcDevice": "Pyke_2006",
+            "srcIfName": "HundredGigE0/0/0/13",
+            "target": 15,
+            "tgtDevice": "UFI_2025",
+            "tgtIfName": "HundredGigE0/0/0/31"
+        },
+        {
+            "id": 15,
+            "source": 15,
+            "srcDevice": "UFI_2025",
+            "srcIfName": "HundredGigE0/0/0/11",
+            "target": 21,
+            "tgtDevice": "Tortin_2019",
+            "tgtIfName": "HundredGigE0/0/1/0"
+        },
+        {
+            "id": 16,
+            "source": 15,
+            "srcDevice": "UFI_2025",
+            "srcIfName": "HundredGigE0/0/0/12",
+            "target": 22,
+            "tgtDevice": "Bigbend_2020",
+            "tgtIfName": "HundredGigE0/0/0/28"
+        },
+        {
+            "id": 17,
+            "source": 15,
+            "srcDevice": "UFI_2025",
+            "srcIfName": "HundredGigE0/0/0/22",
+            "target": 16,
+            "tgtDevice": "Pyke_2007",
+            "tgtIfName": "HundredGigE0/0/0/9"
+        },
+        {
+            "id": 18,
+            "source": 15,
+            "srcDevice": "UFI_2025",
+            "srcIfName": "HundredGigE0/0/0/23",
+            "target": 15,
+            "tgtDevice": "UFI_2025",
+            "tgtIfName": "HundredGigE0/0/0/25"
+        },
+        {
+            "id": 19,
+            "source": 22,
+            "srcDevice": "Bigbend_2020",
+            "srcIfName": "TenGigE0/0/0/4",
+            "target": 18,
+            "tgtDevice": "Bigbend_2021",
+            "tgtIfName": "TenGigE0/0/0/10"
+        },
+        {
+            "id": 20,
+            "source": 22,
+            "srcDevice": "Bigbend_2020",
+            "srcIfName": "TenGigE0/0/0/10",
+            "target": 21,
+            "tgtDevice": "Tortin_2019",
+            "tgtIfName": "TenGigE0/0/0/0"
+        },
+        {
+            "id": 21,
+            "source": 22,
+            "srcDevice": "Bigbend_2020",
+            "srcIfName": "TenGigE0/0/0/11",
+            "target": 23,
+            "tgtDevice": "Tortin_2009",
+            "tgtIfName": "TenGigE0/0/0/1"
+        },
+        {
+            "id": 22,
+            "source": 9,
+            "srcDevice": "Tortin_2008",
+            "srcIfName": "TenGigE0/0/0/0",
+            "target": 24,
+            "tgtDevice": "RSP4_2040_2041",
+            "tgtIfName": "TenGigE0/10/0/0"
+        },
+        {
+            "id": 23,
+            "source": 25,
+            "srcDevice": "J2_modular_2028",
+            "srcIfName": "TenGigE0/1/0/0/0",
+            "target": 26,
+            "tgtDevice": "RSP4_2002_2003",
+            "tgtIfName": "TenGigE0/7/0/1"
+        },
+        {
+            "id": 24,
+            "source": 25,
+            "srcDevice": "J2_modular_2028",
+            "srcIfName": "TenGigE0/1/0/0/1",
+            "target": 27,
+            "tgtDevice": "RSP4_2032",
+            "tgtIfName": "TenGigE0/7/0/6"
+        },
+        {
+            "id": 25,
+            "source": 25,
+            "srcDevice": "J2_modular_2028",
+            "srcIfName": "HundredGigE0/4/0/0",
+            "target": 23,
+            "tgtDevice": "Tortin_2009",
+            "tgtIfName": "HundredGigE0/0/1/0"
+        },
+        {
+            "id": 26,
+            "source": 25,
+            "srcDevice": "J2_modular_2028",
+            "srcIfName": "HundredGigE0/4/0/1",
+            "target": 28,
+            "tgtDevice": "Turin_2004",
+            "tgtIfName": "HundredGigE0/0/1/3"
+        },
+        {
+            "id": 27,
+            "source": 25,
+            "srcDevice": "J2_modular_2028",
+            "srcIfName": "TenGigE0/4/0/2/0",
+            "target": 26,
+            "tgtDevice": "RSP4_2002_2003",
+            "tgtIfName": "TenGigE0/7/0/7"
+        },
+        {
+            "id": 28,
+            "source": 25,
+            "srcDevice": "J2_modular_2028",
+            "srcIfName": "TenGigE0/4/0/2/1",
+            "target": 27,
+            "tgtDevice": "RSP4_2032",
+            "tgtIfName": "TenGigE0/7/0/3"
+        },
+        {
+            "id": 29,
+            "source": 19,
+            "srcDevice": "DDC_2025",
+            "srcIfName": "HundredGigE0/0/0/18",
+            "target": 18,
+            "tgtDevice": "Bigbend_2021",
+            "tgtIfName": "HundredGigE0/0/0/28"
+        },
+        {
+            "id": 30,
+            "source": 19,
+            "srcDevice": "DDC_2025",
+            "srcIfName": "HundredGigE1/0/0/18",
+            "target": 18,
+            "tgtDevice": "Bigbend_2021",
+            "tgtIfName": "HundredGigE0/0/0/29"
+        },
+        {
+            "id": 31,
+            "source": 28,
+            "srcDevice": "Turin_2004",
+            "srcIfName": "TenGigE0/0/0/1",
+            "target": 27,
+            "tgtDevice": "RSP4_2032",
+            "tgtIfName": "TenGigE0/7/0/2"
+        },
+        {
+            "id": 32,
+            "source": 28,
+            "srcDevice": "Turin_2004",
+            "srcIfName": "TenGigE0/0/0/10",
+            "target": 21,
+            "tgtDevice": "Tortin_2019",
+            "tgtIfName": "TenGigE0/0/0/10"
+        },
+        {
+            "id": 33,
+            "source": 28,
+            "srcDevice": "Turin_2004",
+            "srcIfName": "TenGigE0/0/0/15",
+            "target": 23,
+            "tgtDevice": "Tortin_2009",
+            "tgtIfName": "TenGigE0/0/0/15"
+        },
+        {
+            "id": 34,
+            "source": 28,
+            "srcDevice": "Turin_2004",
+            "srcIfName": "HundredGigE0/0/1/2",
+            "target": 16,
+            "tgtDevice": "Pyke_2007",
+            "tgtIfName": "HundredGigE0/0/0/3"
+        },
+        {
+            "id": 35,
+            "source": 24,
+            "srcDevice": "RSP4_2040_2041",
+            "srcIfName": "TenGigE0/10/0/5",
+            "target": 27,
+            "tgtDevice": "RSP4_2032",
+            "tgtIfName": "TenGigE0/15/0/16"
+        },
+        {
+            "id": 36,
+            "source": 21,
+            "srcDevice": "Tortin_2019",
+            "srcIfName": "TenGigE0/0/0/4",
+            "target": 23,
+            "tgtDevice": "Tortin_2009",
+            "tgtIfName": "TenGigE0/0/0/4"
+        },
+        {
+            "id": 37,
+            "source": 21,
+            "srcDevice": "Tortin_2019",
+            "srcIfName": "TenGigE0/0/0/5",
+            "target": 18,
+            "tgtDevice": "Bigbend_2021",
+            "tgtIfName": "TenGigE0/0/0/5"
+        },
+        {
+            "id": 38,
+            "source": 27,
+            "srcDevice": "RSP4_2032",
+            "srcIfName": "TenGigE0/7/0/4",
+            "target": 26,
+            "tgtDevice": "RSP4_2002_2003",
+            "tgtIfName": "TenGigE0/7/0/4"
+        },
+        {
+            "id": 39,
+            "source": 27,
+            "srcDevice": "RSP4_2032",
+            "srcIfName": "TenGigE0/7/0/5",
+            "target": 26,
+            "tgtDevice": "RSP4_2002_2003",
+            "tgtIfName": "TenGigE0/7/0/5"
+        },
+        {
+            "id": 40,
+            "source": 16,
+            "srcDevice": "Pyke_2007",
+            "srcIfName": "HundredGigE0/0/0/4",
+            "target": 16,
+            "tgtDevice": "Pyke_2007",
+            "tgtIfName": "HundredGigE0/0/0/6"
+        }
+    ],
+    "nodes": [
+        {
+            "icon": "nexus5000",
+            "id": 0,
+            "name": "Taihu_2027",
+            "project": "Jarvis-v1/RJIL",
+            "telnetIP": "10.64.103.90",
+            "telnetPort": 2027,
+            "tgen": "Port 15"
+        },
+        {
+            "icon": "dead_node",
+            "id": 1,
+            "name": "Arches_2013",
+            "project": "None",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2013,
+            "tgen": "Te0/0/0/16<->sp 1/4"
+        },
+        {
+            "icon": "dead_node",
+            "id": 2,
+            "name": "J_Modular_2015_2016",
+            "project": "mvpn p0 topology",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2015,
+            "tgen": "Port 8"
+        },
+        {
+            "icon": "dead_node",
+            "id": 3,
+            "name": "RSP4_2034_2035",
+            "project": "BundlePindown/automation/EPRS",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2034,
+            "tgen": "None"
+        },
+        {
+            "icon": "dead_node",
+            "id": 4,
+            "name": "Tortin_2011",
+            "project": "RJIL/MOGRE/ERPS",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2011,
+            "tgen": "Port 17,18"
+        },
+        {
+            "icon": "dead_node",
+            "id": 5,
+            "name": "Tortin_2012",
+            "project": "Jarvis-v1/RJIL",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2012,
+            "tgen": "Port 13,14"
+        },
+        {
+            "icon": "dead_node",
+            "id": 6,
+            "name": "Turin_2014",
+            "project": "MoGRE/RJIL",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2014,
+            "tgen": "Port 16,28"
+        },
+        {
+            "icon": "dead_node",
+            "id": 7,
+            "name": "Zermatt_2010",
+            "project": "standalone",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2010,
+            "tgen": "None"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 8,
+            "name": "RSP4_2039",
+            "project": "MoGRE ",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2039,
+            "tgen": "Port 17,18"
+        },
+        {
+            "icon": "router",
+            "id": 9,
+            "name": "Tortin_2008",
+            "project": "MOGRE",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2008,
+            "tgen": "None"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 10,
+            "name": "Tortin_2005",
+            "project": "ERPS/Automation",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2005,
+            "tgen": "Te0/0/1/0/1<->sp 1/3"
+        },
+        {
+            "icon": "router",
+            "id": 11,
+            "name": "Bigbend_2017",
+            "project": "BundlePindown/automation",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2017,
+            "tgen": "None"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 12,
+            "name": "Eyrie_2023",
+            "project": "None",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2023,
+            "tgen": "Port 2/15"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 13,
+            "name": "Darwin_2024",
+            "project": "2027",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2024,
+            "tgen": "spirent Port 1/2"
+        },
+        {
+            "icon": "router",
+            "id": 14,
+            "name": "UFI_2_2022",
+            "project": "Dual SLC",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2022,
+            "tgen": "None"
+        },
+        {
+            "icon": "router",
+            "id": 15,
+            "name": "UFI_2025",
+            "project": "Dual SLC",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2025,
+            "tgen": "None"
+        },
+        {
+            "icon": "router",
+            "id": 16,
+            "name": "Pyke_2007",
+            "project": "KDDI EVPN",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2007,
+            "tgen": "None"
+        },
+        {
+            "icon": "router",
+            "id": 17,
+            "name": "Tortin_2018",
+            "project": "DDC: AT&T Xleaf",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2018,
+            "tgen": "None"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 18,
+            "name": "Bigbend_2021",
+            "project": "DDC: AT&T Xleaf",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2021,
+            "tgen": "Port 32"
+        },
+        {
+            "icon": "router",
+            "id": 19,
+            "name": "DDC_2025",
+            "project": "None",
+            "telnetIP": "10.64.103.90",
+            "telnetPort": 2025,
+            "tgen": "None"
+        },
+        {
+            "icon": "router",
+            "id": 20,
+            "name": "Pyke_2006",
+            "project": "Dual SLC",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2006,
+            "tgen": "None"
+        },
+        {
+            "icon": "router",
+            "id": 21,
+            "name": "Tortin_2019",
+            "project": "KDDI EVPN",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2019,
+            "tgen": "None"
+        },
+        {
+            "icon": "router",
+            "id": 22,
+            "name": "Bigbend_2020",
+            "project": "KDDI EVPN",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2020,
+            "tgen": "None"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 23,
+            "name": "Tortin_2009",
+            "project": "KDDI EVPN",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2009,
+            "tgen": "Port 9"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 24,
+            "name": "RSP4_2040_2041",
+            "project": "RSP4 setup",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2040,
+            "tgen": "Port 19,20"
+        },
+        {
+            "icon": "router",
+            "id": 25,
+            "name": "J2_modular_2028",
+            "project": "mvpn p0 topology",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2028,
+            "tgen": "None"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 26,
+            "name": "RSP4_2002_2003",
+            "project": "Jarvis-v2",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2002,
+            "tgen": "Port 33,34"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 27,
+            "name": "RSP4_2032",
+            "project": "mvpn p0 topology",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2032,
+            "tgen": "Port 34,35"
+        },
+        {
+            "icon": "nexus5000",
+            "id": 28,
+            "name": "Turin_2004",
+            "project": "KDDI EVPN",
+            "telnetIP": "10.64.103.82",
+            "telnetPort": 2004,
+            "tgen": "Port 10, 11"
+        }
+    ]
+};
+
+topology_timestamp = "11/May/2021 15:22:17"
